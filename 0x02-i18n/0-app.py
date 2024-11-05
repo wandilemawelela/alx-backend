545eb1fca60a6b7aa70633074c785b3017e63035
wandilemawelela <wandile@gmail.com>
2024-11-05 17:14:17 +0000

    [ADD] Fix render index

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,13 +6,17 @@ Sets up a basic Flask application.
 
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    """
+    The home/index page
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
