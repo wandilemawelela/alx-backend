05fe6be6ba06c9e07a768ef9e9a203c113e7c0e0
wandilemawelela <wandile@gmail.com>
2024-11-05 17:08:00 +0000

    [FIX] Styling

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+Sets up a basic Flask application.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
